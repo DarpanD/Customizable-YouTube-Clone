e3ec7d1a84e88e1193f58ec6813ad52f72c3eab6
ddarpan <darpandaswani04@gmail.com>
2024-07-20 22:31:05 +0530

    first commit

---
+++ src/Components/Feed.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import VideoCard from "./VideoCard";
+import { YouTube_API_KEY } from "../apikey.js";
+
+const Feed = ({ Category, RegionCode }) => {
+  const [Data, setData] = useState([]);
+
+  const url =
+    "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48";
+
+  const fetch_date = async () => {
+    await fetch(
+      `${url}&regionCode=${RegionCode}&videoCategoryId=${Category}&key=${YouTube_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((response) => setData(response.items))
+      .catch((error) => console.log("error is  ", error));
+  };
+  useEffect(() => {
+    fetch_date();
+  }, [Category, RegionCode]);
+
+  return (
+    <>
+      <div className="feed flex alc">
+        {Data.map((videoObj) => {
+          return <VideoCard key={videoObj.id} videoObj={videoObj} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Feed;
---
+++ src/apikey.js
@@ -0,0 +1,1 @@
+export const YouTube_API_KEY = "AIzaSyB6bxAN2Kv0N4_lRUv9CAWx_kg4Imh4C9w";
